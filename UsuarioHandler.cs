83dab45b45f3639d488aac7e3016e3600c4a63f3
joseaderr <josefinaalvarezderon@gmail.com>
2023-02-09 20:58:38 -0300

    Update UsuarioHandler.cs

--- UsuarioHandler.cs
+++ UsuarioHandler.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EsqueletoPrimerEntregaJV.Handlers
+namespace PreEntregaJosefinaAlvarez
 {
     internal class UsuarioHandler
     {
@@ -24,17 +25,16 @@ namespace EsqueletoPrimerEntregaJV.Handlers
                     u.Id = reader.GetInt64(0);
                     u.Nombre = reader.GetString(1);
                     u.Apellido = reader.GetString(2);
-                    u.NombreUsuario = reader.GetString(4);
-                    u.Contraseña = reader.GetString(5);
-                    u.Mail = reader.GetString(6);
-
+                    u.Email = reader.GetString(4);
+                    u.UserName = reader.GetString(5);
+                    u.Password = reader.GetString(6);
 
 
                 }
                 return u;
             }
-
-            public Usuario inicioDeSesion(string nombreU, string contraseña)
+        }
+             Usuario inicioDeSesion(String nombreU, String contraseña)
             {
 
                 string cadenaConexion = "Data Source=DESKTOP-GPK2NFU\\SQLEXPRESS;Initial Catalog=SistemaGestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
@@ -43,29 +43,30 @@ namespace EsqueletoPrimerEntregaJV.Handlers
                     SqlCommand comando = new SqlCommand("SELECT FROM Usuario WHERE NombreUsuario = @nombreU AND @Contraseña = @contraseña", conn);
                     conn.Open();
                     SqlDataReader reader = comando.ExecuteReader();
-                    if (reader.HasRows)
+                Usuario u = new Usuario();
+                if (reader.HasRows)
                     {
-                        Usuario u = new Usuario();
+                        
                         reader.Read();
 
                         u.Id = reader.GetInt64(0);
                         u.Nombre = reader.GetString(1);
                         u.Apellido = reader.GetString(2);
-                        u.NombreUsuario = reader.GetString(4);
-                        u.Contraseña = reader.GetString(5);
-                        u.Mail = reader.GetString(6);
+                        u.Email = reader.GetString(4);
+                        u.UserName = reader.GetString(5);
+                        u.Password = reader.GetString(6);
 
-                        return u;
-
-                    }
-
+                   
                 }
+                return u;
+            }
 
+                
             }
 
 
 
-        }
+        
 
     }
 }
